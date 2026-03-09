08107411ff68beb154f6dc832f4076543e10fc5b
ZambrzyckiBartosz <zambrzyckibartosz709@gmail.com>
2026-03-09 18:33:03 +0100

    Lab1

--- Lab1/for_students.py
+++ Lab1/for_students.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from ans import x_standarized, learning_rate, y_pred
 from data import get_data, inspect_data, split_data
 
 data = get_data()
@@ -9,14 +8,6 @@ inspect_data(data)
 
 train_data, test_data = split_data(data)
 
-# Simple Linear Regression
-# predict MPG (y, dependent variable) using Weight (x, independent variable) using closed-form solution
-# y = theta_0 + theta_1 * x - we want to find theta_0 and theta_1 parameters that minimize the prediction error
-
-# We can calculate the error using MSE metric:
-# MSE = SUM (from i=1 to n) (actual_output - predicted_output) ** 2
-
-# get the columns
 y_train = train_data['MPG'].to_numpy()
 x_train = train_data['Weight'].to_numpy()
 
@@ -24,19 +15,17 @@ y_test = test_data['MPG'].to_numpy()
 x_test = test_data['Weight'].to_numpy()
 
 # TODO: calculate closed-form solution
-theta_best = [0,0]
 
 m = x_train.size
-x_matrix = np.concatenate((np.ones((m,1)),x_train.reshape(-1,1)),1)
-y_matrix = y_train.reshape(-1,1)
+x_matrix = np.concatenate((np.ones((m, 1)), x_train.reshape(-1, 1)), 1)
+y_matrix = y_train.reshape(-1, 1)
 theta_matrix = np.linalg.inv(x_matrix.T.dot(x_matrix)).dot(x_matrix.T).dot(y_matrix)
-thehta_best = theta_matrix.flatten()
+theta_best = theta_matrix.flatten()
+
 # TODO: calculate error
 
 mse = np.mean(((theta_best[1] * x_test + theta_best[0]) - y_test) ** 2)
-
-# plot the regression line
-
+print("MSE:", mse)
 
 x = np.linspace(min(x_test), max(x_test), 100)
 y = float(theta_best[0]) + float(theta_best[1]) * x
@@ -47,39 +36,47 @@ plt.ylabel('MPG')
 plt.show()
 
 print()
+
 # TODO: standardization
+
 x_mean = np.mean(x_train)
+x_std  = np.std(x_train)
 y_mean = np.mean(y_train)
-x_std = np.std(x_train)
-y_std = np.std(y_train)
+y_std  = np.std(y_train)
 
 x_train = (x_train - x_mean) / x_std
 y_train = (y_train - y_mean) / y_std
-x_matrix = np.concatenate((np.ones((m,1)),x_train.reshape(-1,1)),1)
-y_matrix = y_train.reshape(-1,1)
 
-theta_best = np.random.rand(1,2).reshape(-1,1)
-learning_rate = 0.001
-max_iter = 1000
-for i in range(max_iter):
-    gradient = (2/m) * x_matrix.T.dot(x_matrix.dot(theta_best) - y_matrix)
-    theta_best = theta_best - learning_rate * gradient
-
-theta_best = theta_best.flatten()
-
+x_matrix = np.concatenate((np.ones((m, 1)), x_train.reshape(-1,1)), 1)
+y_matrix = y_train.reshape(-1, 1)
 
 # TODO: calculate theta using Batch Gradient Descent
+
+theta_best = np.random.rand(1, 2).reshape(-1, 1)
+learning_rate = 0.001
+max_epoch = 100000
+
+for i in range(max_epoch):
+    gradientMse = (2/m) * x_matrix.T.dot(x_matrix.dot(theta_best) - y_matrix)
+    theta_best = theta_best - learning_rate * gradientMse
+
+theta_best = theta_best.flatten()
 
 # TODO: calculate error
 
 x_standarized = (x_test - x_mean) / x_std
-y_standarized = (y_test - y_mean) / y_std
-y_pred = (y_standarized * y_std) * y_mean
+y_result = theta_best[1] * x_standarized + theta_best[0]
+y_prediction = (y_result * y_std) + y_mean
 
-mse = np.mean((y_pred - y_std) ** 2)
-# plot the regression line
+mse = np.mean((y_prediction - y_test) ** 2)
+
+print("MSE2:", mse)
+
+
 x = np.linspace(min(x_test), max(x_test), 100)
-y = float(theta_best[0]) + float(theta_best[1]) * x
+x_standarized = (x - x_mean) / x_std
+y_standarized = float(theta_best[0]) + float(theta_best[1]) * x_standarized
+y = (y_standarized * y_std) + y_mean
 plt.plot(x, y)
 plt.scatter(x_test, y_test)
 plt.xlabel('Weight')
